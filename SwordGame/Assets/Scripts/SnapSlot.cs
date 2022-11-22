cef500363db099106afe3e54f4d39e8031bf2b93
Alperen Onal <alperenonal9922@gmail.com>
2022-11-22 12:59:58 +0300

    Smoothing the snapping process (Coroutine)
    + smoothing the snapping process with coroutine

--- SwordGame/Assets/Scripts/SnapSlot.cs
+++ SwordGame/Assets/Scripts/SnapSlot.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class SnapSlot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, .75f);
     }
 }
--- SwordGame/Assets/Scripts/Snapper.cs
+++ SwordGame/Assets/Scripts/Snapper.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Snapper : MonoBehaviour
 {
     public Transform snapSlotTransform;
+    public float placementTime = 1f;
     private bool moving;
     public bool snapped;
     Camera viewCam;
@@ -25,7 +26,7 @@ public class Snapper : MonoBehaviour
             {
                 Vector3 mousePos = Input.mousePosition;
                 mousePos = viewCam.ScreenToWorldPoint(mousePos);
-
+                // dragging
                 // baslangic vectorlerini cikarmazsan her tikladiginda kilicin ortasi hep mouse pos'a isinlaniyor
                 // koddan anlasilacagi uzere kilicin transform.position'u mousenin oldugu yer oluyor
                 transform.position = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, transform.position.z);
@@ -56,20 +57,34 @@ public class Snapper : MonoBehaviour
         {
             moving = false;
             // makin square 0.6 unit = .6 * .6 = .36
-            if (Vector3.SqrMagnitude(transform.position - snapSlotTransform.position) < .36f)
+            if (Vector3.SqrMagnitude(transform.position - snapSlotTransform.position) < .5625f)
             {
                 print(Vector3.SqrMagnitude(transform.position - snapSlotTransform.position));
                 // make these with coroutines (transform and rotation)
-                transform.position = snapSlotTransform.position;
-                transform.rotation = snapSlotTransform.rotation;
+                //transform.position = snapSlotTransform.position;
+                //transform.rotation = snapSlotTransform.rotation;
+                StartCoroutine(AnimateSwordPosition());
                 transform.parent = snapSlotTransform;
                 snapped = true;
             }
         }
     }
 
-    private void OnDrawGizmos()
+    IEnumerator AnimateSwordPosition()
     {
-        Gizmos.DrawSphere(snapSlotTransform.position, .6f);
+        float placementSpeed = 1f / placementTime;
+        float percent = 0;
+        Vector3 initialPos = transform.position;
+        Quaternion initialRotation = transform.rotation;
+
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * placementSpeed;
+            transform.position = Vector3.Lerp(initialPos, snapSlotTransform.position, percent);
+            transform.rotation = Quaternion.Lerp(initialRotation, snapSlotTransform.rotation, percent);
+            yield return null;
+        }
+        //transform.position = snapSlotTransform.position;
+        //transform.rotation = snapSlotTransform.rotation;
     }
 }
